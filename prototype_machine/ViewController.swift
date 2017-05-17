0d190e06baa35788c5c4f3a0daa4a11ddeb65ffd
Kelong Wu <kwu18@illinois.edu>
2017-05-17 16:43:08 -0500

    Initial Commit

---
+++ prototype_machine/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  prototype_machine
+//
+//  Created by Kelong Wu on 5/17/17.
+//  Copyright © 2017 alien_robot_cat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
